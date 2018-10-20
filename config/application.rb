79d53b7c92f6048b4f2428dad83cb55759fa8257
hyuga <hyuga3110@gmail.com>
2018-10-20 12:53:22 +0900

    haml対応＆rspecのgenerator設定

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,24 @@ module WorkReact
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+
+      # rspecの自動生成
+      g.test_framework :rspec,
+        fixture: true,
+        fixture_replacement: :factory_bot,
+        view_specs: false,
+        routing_specs: false,
+        helper_specs: false,
+        controller_specs: false,
+        integration_tool: false
+    
+      # fixtureの代わりにfactory_girlを使うよう設定
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+      
+    end
   end
 end
